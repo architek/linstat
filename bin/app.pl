99c6a83992e87fc64f6ae499e9377e6a6921ea4b
Laurent Kislaire <teebeenator@gmail.com>
2015-03-28 00:41:59 +0100

    Started fork of pistat for Linux

--- bin/app.pl
+++ bin/app.pl
@@ -1,16 +1,13 @@
 #!/usr/bin/env perl
 use Dancer;
-use pista;
-use Device::BCM2835;
+use linstat;
 
-my $pid_file_name="/tmp/pistat.pid";
+my $pid_file_name="/tmp/linstat.pid";
 
 sub got_sig {
   unlink $pid_file_name;
   exit(0);
 }
-
-Device::BCM2835::init() || die "Could not init BCM2835 library";
 
 open my $pid_file , ">" , $pid_file_name;
 print $pid_file "$$\n";
